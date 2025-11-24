f20a97ae74bbc614f1282aa469b47bb35d73c204
Darius Houle <darius.houle@trailofbits.com>
2025-11-24 09:12:35 -0700

    fix: watch broken on modern esbuild

--- esbuild.js
+++ esbuild.js
@@ -1,4 +1,4 @@
-const { build } = require("esbuild");
+const { build, context } = require("esbuild");
 
 const baseConfig = {
     bundle: true,
@@ -17,21 +17,6 @@ const extensionConfig = {
     external: ["vscode"],
 };
 
-const watchConfig = {
-    watch: {
-        onRebuild(error, result) {
-            console.log("[watch] build started");
-            if (error) {
-                error.errors.forEach((error) =>
-                    console.error(`> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`),
-                );
-            } else {
-                console.log("[watch] build finished");
-            }
-        },
-    },
-};
-
 const webviewConfig = {
     ...baseConfig,
     target: "es2020",
@@ -48,18 +33,40 @@ const gitConfigWebviewConfig = {
     outfile: "./out/gitConfigWebview.js",
 };
 
+const watchPlugin = {
+    name: "watch-plugin",
+    setup(build) {
+        build.onStart(() => {
+            console.log(`[watch] build started (${build.initialOptions.outfile})`);
+        });
+
+        build.onEnd((result) => {
+            console.log(`${result.errors.length > 0 ? "[watch] build finished with errors" : "[watch] build finished successfully"} (${build.initialOptions.outfile})`);
+        });
+    },
+};
+
 (async () => {
     const args = process.argv.slice(2);
     try {
         if (args.includes("--watch")) {
             // Build and watch extension and webview code
-            console.log("[watch] build started");
+            const extensionContext = await context({
+                ...extensionConfig,
+                plugins: [watchPlugin],
+            });
+            const webviewContext = await context({
+                ...webviewConfig,
+                plugins: [watchPlugin],
+            });
+            const gitConfigWebviewContext = await context({
+                ...gitConfigWebviewConfig,
+                plugins: [watchPlugin],
+            });
 
-            await build({ ...extensionConfig, ...watchConfig });
-            await build({ ...webviewConfig, ...watchConfig });
-            await build({ ...gitConfigWebviewConfig, ...watchConfig });
-
-            console.log("[watch] build finished");
+            await extensionContext.watch();
+            await webviewContext.watch();
+            await gitConfigWebviewContext.watch();
         } else {
             // Build extension and webview code
             await build(extensionConfig);
