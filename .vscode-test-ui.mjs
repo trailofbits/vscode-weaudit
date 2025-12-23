26cd4dded169584a1539a1929e0e623ba0f2a871
Filipe Casal <fcasal@users.noreply.github.com>
2025-12-23 18:36:04 +0000

    feat: add UI tests with vscode-extension-tester

---
+++ .vscode-test-ui.mjs
@@ -0,0 +1,11 @@
+export default {
+    vscodeVersion: "stable",
+    extensions: [],
+    settings: {
+        mocha: {
+            ui: "bdd",
+            timeout: 20000,
+            color: true,
+        },
+    },
+};
